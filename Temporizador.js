c71ad614cef7745def6010891a601ef8923e78f8
Álvaro Sierra <asierraperez@danielcastelao.org>
2022-03-22 18:10:48 +0100

    ej5

--- Temporizador.js
+++ Temporizador.js
@@ -1,13 +1,18 @@
 function iniciaTemporizador() {
 
     var inicio_Temp = document.getElementById("start_temporizador")
+    var tiempo_temp = document.getElementById("text_temporizador")
 
     inicio_Temp.addEventListener("click", (evt) => {
         var horas = document.getElementById("horas_temporizador").value
         var min = document.getElementById("minutos_temporizador").value
         var seg = document.getElementById("segundos_temporizador").value
-        console.log("horas: " + horas
-            + "\nminutos: " + min
-            + "\nsegundos: " + seg)
+        var aux_hora, aux_min, aux_seg
+
+        seg > 10 ? aux_seg = seg : aux_seg = "0" + seg
+        min > 10 ? aux_min = m : aux_min = "0" + min
+        horas > 10 ? aux_hora = horas : aux_hora = "0" + horas
+
+        tiempo_temp.innerHTML = + aux_hora + ":" + aux_min + ":" + aux_seg
     })
 }
